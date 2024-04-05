e174c4538679ab2bb836b547b62538a886a6a279
annanas-dev <annanasidletskaia@icloud.com>
2024-04-05 23:03:56 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Lab_KeyboardHook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.start = new System.Windows.Forms.Button();
+            this.stop = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(190, 170);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(247, 78);
+            this.start.TabIndex = 0;
+            this.start.Text = "Запуск";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // stop
+            // 
+            this.stop.Location = new System.Drawing.Point(492, 170);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(247, 78);
+            this.stop.TabIndex = 1;
+            this.stop.Text = "Стоп";
+            this.stop.UseVisualStyleBackColor = true;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(190, 286);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(549, 26);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(190, 348);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(549, 26);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1325, 869);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.start);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using KeyboardHookDLL;
+using MouseHookDLL;
+
+
+namespace Lab_KeyboardHook
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            KeyboardHook.KeyPressed += KeyboardHook_KeyPressed;
+            MouseHook.MouseClicked += MouseHook_MouseClicked;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            KeyboardHook.Start();
+            textBox1.AppendText("WH_KEYBOARD started." + Environment.NewLine);
+
+            MouseHook.Start();
+            textBox2.AppendText("WH_MOUSE started." + Environment.NewLine);
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            KeyboardHook.Stop();
+            textBox1.Clear();
+            textBox1.AppendText("WH_KEYBOARD stopped." + Environment.NewLine);
+
+            MouseHook.Stop();
+            textBox2.Clear();
+            textBox2.AppendText("WH_MOUSE stopped." + Environment.NewLine);
+        }
+
+        private void KeyboardHook_KeyPressed(object sender, KeyboardHook.KeyEventArgs e)
+        {
+            textBox1.AppendText(e.Key.ToString() + Environment.NewLine);
+        }
+
+        private void MouseHook_MouseClicked(object sender, MouseHook.MouseEventArgs e)
+        {
+            textBox2.Invoke((MethodInvoker)delegate
+            {
+                textBox2.AppendText($"X: {e.X}, Y: {e.Y}" + Environment.NewLine);
+            });
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
